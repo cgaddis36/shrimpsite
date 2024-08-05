6c36e720d70de345e1d65a4f9e183229879772a1
Chase Gaddis <cgaddis36@gmail.com>
2024-08-05 12:18:31 -0400

    category model validations and relationships

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :items
+  has_many :brands, :through => :items 
+  validates_presence_of :title
+  validates_uniqueness_of :title
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do 
+    it { should have_many :items }
+    it { should have_many(:brands).through(:items) }
+  end 
+  describe "validations" do 
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
+  end 
 end
