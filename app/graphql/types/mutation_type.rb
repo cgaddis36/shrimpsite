72c2a1209da31de4f29c34efe598e3121a4dcb98
Chase Gaddis <cgaddis36@gmail.com>
2024-08-05 16:52:40 -0400

    Add fields for update, destroy Item

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_item, mutation: Mutations::Items::Create
+    field :update_item, mutation: Mutations::Items::Update
+    field :destroy_item, mutation: Mutations::Items::Destroy
   end
 end
