86b86fb49e13a7917fd6e0b23cf4e80b52d16fda
Chase Gaddis <cgaddis36@gmail.com>
2024-08-05 17:34:07 -0400

    add fields to query_type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,5 +5,7 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
     field :fetch_all_items, resolver: Queries::Items::FetchAll
+    field :fetch_by_brand, resolver: Queries::Items::FetchByBrand
+    field :fetch_by_category, resolver: Queries::Items::FetchByCategory
   end
 end
