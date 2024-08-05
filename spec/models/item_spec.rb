9a7aa744095989a7b0a5f148a0395fd2c02dc713
Chase Gaddis <cgaddis36@gmail.com>
2024-08-05 12:20:50 -0400

    Item model validations and relationships

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do 
+    it { should belong_to :category }
+    it { should belong_to :brand }
+  end 
+  describe "validations" do 
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:quantity) }
+  end 
 end
